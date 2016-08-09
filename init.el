a639e2e2acc5c323c26dd12eb85f61b1f2460889
msorce <marksorce@gmail.com>
2016-08-09 13:16:00 -0700

    spaceline

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+;; more repos
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
@@ -5,15 +6,18 @@
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (package-initialize)
 
+;; if !use-package => install
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
+;; init use-package
 (eval-when-compile
   (require 'use-package))
 (require 'diminish)
 (require 'bind-key)
 
+;; less gui
 (setq initial-scratch-message "")
 (setq inhibit-splash-screen t)
 (load-theme 'misterioso)
@@ -21,6 +25,12 @@
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 
+;; evil mode
 (use-package evil
   :config
   (evil-mode 1))
+
+;; spaceline
+(use-package spaceline-config
+  :config
+  (spaceline-emacs-theme))
