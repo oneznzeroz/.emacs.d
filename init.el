8ec36101a72a4ea4c74b0a019396c3c2a221ba94
msorce <marksorce@gmail.com>
2016-08-09 11:59:50 -0700

    packages

--- init.el
+++ init.el
@@ -13,3 +13,14 @@
   (require 'use-package))
 (require 'diminish)
 (require 'bind-key)
+
+(setq initial-scratch-message "")
+(setq inhibit-splash-screen t)
+(load-theme 'misterioso)
+(scroll-bar-mode 0)
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+
+(use-package evil
+  :config
+  (evil-mode 1))
