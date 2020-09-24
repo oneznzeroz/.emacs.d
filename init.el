bb75feb10e6783433fc07a364ee63ccdba12cc0b
Roger Nolasco <roger@xrc.io>
2020-09-24 01:12:10 -0700

    test

--- init.el
+++ init.el
@@ -1,128 +1,119 @@
-;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;;
-;;                  ___                                   ___                    ;;
-;;    ___          /__/\        ___           ___        /  /\                   ;;
-;;   /  /\         \  \:\      /  /\         /  /\      /  /:/_                  ;;
-;;  /  /:/          \  \:\    /  /:/        /  /:/     /  /:/ /\    ___     ___  ;;
-;; /__/::\      _____\__\:\  /__/::\       /  /:/     /  /:/ /:/_  /__/\   /  /\ ;;
-;; \__\/\:\__  /__/::::::::\ \__\/\:\__   /  /::\    /__/:/ /:/ /\ \  \:\ /  /:/ ;;
-;;    \  \:\/\ \  \:\~~\~~\/    \  \:\/\ /__/:/\:\   \  \:\/:/ /:/  \  \:\  /:/  ;;
-;;     \__\::/  \  \:\  ~~~      \__\::/ \__\/  \:\   \  \::/ /:/    \  \:\/:/   ;;
-;;     /__/:/    \  \:\          /__/:/       \  \:\   \  \:\/:/      \  \::/    ;;
-;;     \__\/      \  \:\         \__\/         \__\/    \  \::/        \__\/     ;;
-;;                 \__\/                                 \__\/                   ;;
-;;                      ~ Configuration by: Mark Sorce ~                         ;;
-;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;; ;;
-
-;; more repos
+;; ADD REPOS
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
 (package-initialize)
 
 ;; if !use-package => install
 (unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+    (package-refresh-contents)
+    (package-install 'use-package))
 
 ;; init use-package
 (eval-when-compile
-  (require 'use-package))
-(require 'diminish)
-(require 'bind-key)
+    (require 'use-package))
 
 ;; less gui
 (setq initial-scratch-message "")
-(setq inhibit-splash-screen t)
-(scroll-bar-mode 0)
-(menu-bar-mode 0)
+;; (setq inhibit-splash-screen t)
+;; (scroll-bar-mode 0)
+;;(menu-bar-mode 0)
 (tool-bar-mode 0)
 
-;;(set-frame-parameter (selected-frame) 'alpha '(<active> . <inactive>))
-;;(set-frame-parameter (selected-frame) 'alpha <both>)
-(set-frame-parameter (selected-frame) 'alpha '(85 . 50))
-(add-to-list 'default-frame-alist '(alpha . (85 . 50)))
+;; disable mouse
+;(use-package disable-mouse
+;  :ensure t
+;  :config
+;  (global-disable-mouse-mode))
+
+;; transparency
+;(set-frame-parameter (selected-frame) 'alpha '(<active> . <inactive>))
+;(set-frame-parameter (selected-frame) 'alpha <both>)
+;(set-frame-parameter (selected-frame) 'alpha '(85 . 50))
+;(add-to-list 'default-frame-alist '(alpha . (85 . 50)))
+
+;; backups
+;(setq make-backup-files nil) ; stop creating backup~ files
+;(setq auto-save-default nil) ; stop creating #autosave# files
+(setq backup-directory-alist '(("" . "~/.emacs.d/backup")))
+
+;; unset C- and M- digit keys
+(dotimes (n 10)
+  ;(global-unset-key (kbd (format "C-%d" n)))
+  (global-unset-key (kbd (format "M-%d" n)))
+  )
+
+;; window mgmt
+(use-package eyebrowse
+  :diminish eyebrowse-mode
+  :config (progn
+  (define-key eyebrowse-mode-map (kbd "M-1") 'eyebrowse-switch-to-window-config-1)
+  (define-key eyebrowse-mode-map (kbd "M-2") 'eyebrowse-switch-to-window-config-2)
+  (define-key eyebrowse-mode-map (kbd "M-3") 'eyebrowse-switch-to-window-config-3)
+  (define-key eyebrowse-mode-map (kbd "M-4") 'eyebrowse-switch-to-window-config-4)
+  (eyebrowse-mode t)
+  (setq eyebrowse-new-workspace t))
+  )
+
 
 ;highlights
 ;(highlight-tabs)
 ;(highlight-trailing-whitespace)
 
-(use-package magit
-  :ensure t
-  :config )
-
-;; backups
-(setq make-backup-files nil) ; stop creating backup~ files
-(setq auto-save-default nil) ; stop creating #autosave# files
-(setq backup-directory-alist '(("" . "~/.emacs.d/emacs-backup")))
-;(setq backup-directory-alist
-;`((".*" . ,temporary-file-directory)))
-;(setq auto-save-file-name-transforms
-;`((".*" ,temporary-file-directory t)))
-
-;; handle prompts
-(fset 'yes-or-no-p 'y-or-n-p)
-(setq confirm-nonexistent-file-or-buffer nil)
-(setq kill-buffer-query-functions
-      (remq 'process-kill-buffer-query-function
-            kill-buffer-query-functions))
-
-;; Electric
-(electric-indent-mode 1)
-(electric-pair-mode 1)
-
 ;; commenting
 (global-set-key (kbd "C-;") 'comment-or-uncomment-region)
 
 ;; tabs => 2 spaces
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 2)
+;(setq-default indent-tabs-mode nil)
+;(setq-default tab-width 2)
 ;; show shitespace
-(global-whitespace-mode 1)
+;(global-whitespace-mode 1)
 
-(defvar html-indent-offset)
-(setq html-indent-offset 2)
-
-(defvar js-indent-level)
-(setq js-indent-level 2)
-
-(defvar css-indent-offset)
-(setq css-indent-offset 2)
-
-(defvar javascript-indent-level)
-(setq javascript-indent-level 2)
-
-(defvar scss-indent-offset)
-(setq scss-indent-offset 2)
-
-;; smooth scrolling
-(setq scroll-conservatively 101) ;; move minimum when cursor exits view, instead of recentering
-(setq mouse-wheel-scroll-amount '(1)) ;; mouse scroll moves 1 line at a time, instead of 5 lines
-(setq mouse-wheel-progressive-speed nil) ;; on a long mouse scroll keep scrolling by 1 line
-(setq scroll-margin 5)
-
-;; evil mode
-(use-package evil
+;; GIT
+(use-package magit
   :ensure t
-  :config
-  (evil-mode 1))
+  :config )
 
-;; evil surround
-(use-package evil-surround
+;; THEME
+(use-package spacemacs-common
+    :ensure spacemacs-theme
+    :config (load-theme 'spacemacs-dark t))
+
+(use-package spaceline
   :ensure t
+  :init
+  (setq powerline-default-separator 'arrow-fade)
   :config
-  (global-evil-surround-mode 1))
-;; web mode
-(use-package web-mode
-  :ensure t
-  :config
-  (setq web-mode-markup-indent-offset 2)
-  (set-face-background 'web-mode-current-element-highlight-face "yellow")
-  (set-face-attribute 'web-mode-html-tag-face nil :foreground "magenta")
-  (set-face-attribute 'web-mode-html-attr-name-face nil :foreground "pink")
-  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-  (setq web-mode-enable-current-element-highlight t))
+  (require 'spaceline-config)
+  (spaceline-spacemacs-theme))
+
+;(use-package dracula-theme
+;  :ensure t
+;  :config
+;  (load-theme 'dracula t))
+
+;; window and frame numbering
+(use-package winum
+    :ensure winum
+    :config 
+    (setq winum-auto-setup-mode-line nil)
+    (winum-mode))
+
+(use-package anzu
+  :ensure anzu
+  :init
+  (setq anzu-cons-mode-line-p nil))
+
+
+(use-package anzu
+  :ensure anzu
+  :init
+  (setq anzu-cons-mode-line-p nil))
+
+;;use larger font
+(setq default-frame-alist '((font . "Ubuntu Mono-16")))
 
 ;; linum rel
 (use-package linum-relative
@@ -152,36 +143,10 @@
   (helm-projectile-on)
   (setq projectile-indexing-method 'alien))
 
-;; spaceline
-(use-package spaceline
-  :ensure t)
-(use-package spaceline-config
-  :config
-  (set-face-attribute 'spaceline-evil-normal nil :foreground "black")
-  (spaceline-spacemacs-theme))
-;; theme
-(use-package cyberpunk-theme
-  :ensure t
-  :config
-  )
-;; (use-package moe-theme
-;;   :ensure t
-;;   :config
-;;   (load-theme 'moe-dark t)
-;;   (moe-theme-set-color 'green)
-;;   (powerline-moe-theme))
 ;; nyan mode
-(use-package nyan-mode
-  :ensure t
-  :init (nyan-mode))
-
-;; sass
-(use-package scss-mode
-  :ensure t
-  :config
-  (autoload 'scss-mode "scss-mode")
-  (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
-  (add-to-list 'auto-mode-alist '("\\.sass\\'" . scss-mode)))
+;(use-package nyan-mode
+;  :ensure t
+;  :init (nyan-mode))
 
 ;; company
 (use-package company
@@ -191,18 +156,28 @@
   (setq company-minimum-prefix-length 1)
   :init (global-company-mode))
 
-;; company-tern
-(use-package company-tern
-  :ensure t
-  :init (add-to-list 'company-backends 'company-tern))
-
 ;; yasnippet
-(use-package yasnippet
-  :ensure t
-  :config
-  (yas-global-mode 1))
+;(use-package yasnippet
+;  :ensure t
+;  :config
+;  (yas-global-mode 1))
 
 ;; flycheck
 (use-package flycheck
   :ensure t
   :init (global-flycheck-mode))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("b89a4f5916c29a235d0600ad5a0849b1c50fab16c2c518e1d98f0412367e7f97" default))
+ '(package-selected-packages '(eyebrowse magit cyberpunk-theme use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
